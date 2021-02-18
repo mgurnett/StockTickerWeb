b6e5742ab584076be2effcde0c94349bf1edc6a1
Michael Gurnett <mgurnett@gmail.com>
2021-02-17 21:28:44 -0700

    working on NHL

--- nhlgames.py
+++ nhlgames.py
@@ -1,10 +1,11 @@
 import requests
+from datetime import datetime
 
 # Set up the API call variables
 game_data = []
 year = '2020'
 season_type = '02' 
-max_game_ID = 20
+max_game_ID = 30
 base_URL = "https://statsapi.web.nhl.com/api/v1/"
 
 def read_API (section):
@@ -21,17 +22,16 @@ for i in range(1,max_game_ID):
     url='game/' + year + season_type +str(i).zfill(4) + '/linescore'
     data = read_API (url)
     game_data.append(data)
-    
-print (type(game_data))
-one_game = game_data[18]
-print(type(one_game))
-# for x in one_game.keys():
-#   print(x)
-# for x in one_game.values():
-#   print(x)
-home = one_game['teams']['home']['team']['name']
-h_score = one_game['teams']['home']['goals']
-away = one_game['teams']['away']['team']['name']
-a_score = one_game['teams']['away']['goals']
-print (f'The {home} scored {h_score} and the {away} scored {a_score}')
-  
+    home = data['teams']['home']['team']['name']
+    h_score = data['teams']['home']['goals']
+    away = data['teams']['away']['team']['name']
+    a_score = data['teams']['away']['goals']
+    date_data = data['periods']
+    try:
+        date_data = data['periods'][0]['startTime']
+    except:
+      datetime_object = datetime.now()  
+    date_data = data['periods'][0]['startTime']
+    datetime_object = datetime.strptime(date_data, '%Y-%m-%dT%H:%M:%SZ')
+    print (f'On {datetime_object.strftime("%a, %b %d")} the {home} scored {h_score} and the {away} scored {a_score}')
+#     print (f'On the {home} scored {h_score} and the {away} scored {a_score}')
