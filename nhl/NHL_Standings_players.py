dbfdde8699a530d4a2aca6bcceff64130202ce7c
Michael <mgurnett@gmail.com>
2021-03-01 16:45:47 -0700

    trying a new way of dealing with lists only and THEN changing to dataframe

--- nhl/NHL_Standings_players.py
+++ nhl/NHL_Standings_players.py
@@ -49,74 +49,76 @@ def get_rosters (t_id):
                 active_roster.append (r)
     return active_roster
 
+def get_player_data (url):
+#     print (url)
+    r = requests.get(url)
+    if r.status_code != 200:
+        print (f"status code is {r.status_code}")
+    else:
+        data = r.json()
+    return data
+
 def get_player_info (id):
-    url = base_URL + '/people/' + str(id)
-    r = requests.get(url)
-    if r.status_code != 200:
-        print (f"status code is {r.status_code}")
-    else:
-        data = r.json()
-        players_df_full = pd.json_normalize(data, ['people'], errors='ignore')
-#         make_web ('players' + str(id), players_df_full)
-#         print (players_df_full.info())
-        player_part_dict = {'id': players_df_full['id'],
-                            'firstName': players_df_full['firstName'],
-                            'lastName': players_df_full['lastName'],
-                            'primaryNumber': players_df_full['primaryNumber'],
-                            'currentAge': players_df_full['currentAge'],
-                            'nationality': players_df_full['nationality'],
-                            'active': players_df_full['active'],
-                            'alternateCaptain': players_df_full['alternateCaptain'],
-                            'captain': players_df_full['captain'],
-                            'rosterStatus': players_df_full['rosterStatus'],
-                            'currentTeam.id': players_df_full['currentTeam.id'],
-                            'primaryPosition.name': players_df_full['primaryPosition.name']  }
-#         players_df = pd.DataFrame.from_dict(player_part_dict, orient='index')
-#     return players_df
-    return player_part_dict
-
-def get_player_stats (id):
+    #Player stats
     url = base_URL + '/people/' + str(id) + '/stats?stats=statsSingleSeason&season=20202021'
-    r = requests.get(url)
-    if r.status_code != 200:
-        print (f"status code is {r.status_code}")
-    else:
-        data = r.json()
-        players_df = pd.json_normalize(data, ['stats'], errors='ignore')
-        if players_df['splits'][0] != []:
-#             player_info_df = get_player_info (id)
-            player_info_dict = get_player_info (id)
-            print (player_info_dict)
-#             if player_info_dict['primaryPosition.name'] == 'goalie':
-            temp= players_df['splits'][0][0]['stat']
-
-            temp_df = pd.DataFrame.from_dict(temp, orient='index')
-            temp2_df = temp_df.transpose()
-            
-            player_part_dict = {'assists': temp2_df['assists'],
-                                'goals': temp2_df['goals'],
-                                'pim': temp2_df['pim'],
-                                'shots': temp2_df['shots'],
-                                'games': temp2_df['games'],
-                                'powerPlayGoals': temp2_df['powerPlayGoals'],
-                                'powerPlayPoints': temp2_df['powerPlayPoints'],
-                                'penaltyMinutes': temp2_df['penaltyMinutes'],
-                                'plusMinus': temp2_df['plusMinus'],
-                                'points': temp2_df['points'] }
-
-            temp3_df = pd.DataFrame.from_dict(player_part_dict, orient='index')
-
-    player_details = pd.concat([player_info_df, temp3_df], axis=1)
-    return player_details
+    data = get_player_data (url) # data is a <class 'dict'>
+    data_stats = data['stats'][0]#; print ('data_stats', type(data_stats), data_stats)
+    data_splits = data_stats['splits']#; print ('data_splits', type(data_splits), data_splits)
+    
+    if data_splits != []:
+        data_splits = data_stats['splits'][0]#; print ('data_splits', type(data_splits), data_splits)
+        data_stat = data_splits['stat']#; print ('data_stat', type(data_stat), data_stat.keys())
+        print (data_stat.keys())
+        print ('============')
+#     players_stats_df = pd.json_normalize(data, ['stats'], errors='ignore')
+#     if players_stats_df['splits'][0] != []:
+#         temp_df = players_stats_df['splits'][0][0]['stat']
+#         url = base_URL + '/people/' + str(id)
+#         data = get_player_data (url)
+#         players_info_df = pd.json_normalize(data, ['people'], errors='ignore')
+#         
+#         temp_df = pd.DataFrame.from_dict(temp, orient='index')
+#             temp2_df = temp_df.transpose()
+#         
+#         player_info_dict = {'id': players_info_df['id'],
+#                             'firstName': players_info_df['firstName'],
+#                             'lastName': players_info_df['lastName'],
+#                             'primaryNumber': players_info_df['primaryNumber'],
+#                             'currentAge': players_info_df['currentAge'],
+#                             'nationality': players_info_df['nationality'],
+#                             'active': players_info_df['active'],
+#                             'alternateCaptain': players_info_df['alternateCaptain'],
+#                             'captain': players_info_df['captain'],
+#                             'rosterStatus': players_info_df['rosterStatus'],
+#                             'currentTeam.id': players_info_df['currentTeam.id'],
+#                             'primaryPosition.name': players_info_df['primaryPosition.name']  }
+#         
+#         player_stats_dict = {'assists': temp2_df['assists'],
+#                                 'goals': temp2_df['goals'],
+#                                 'pim': temp2_df['pim'],
+#                                 'shots': temp2_df['shots'],
+#                                 'games': temp2_df['games'],
+#                                 'powerPlayGoals': temp2_df['powerPlayGoals'],
+#                                 'powerPlayPoints': temp2_df['powerPlayPoints'],
+#                                 'penaltyMinutes': temp2_df['penaltyMinutes'],
+#                                 'plusMinus': temp2_df['plusMinus'],
+#                                 'points': temp2_df['points'] }
+# 
+#             temp3_df = pd.DataFrame.from_dict(player_part_dict, orient='index')
+# 
+#     player_details = pd.concat([player_info_df, temp3_df], axis=1)
+#     return player_details
+    return data
         
 team_ids = get_teams(); print (f'Got the team ids for {len(team_ids)} teams')
 roster_ids = get_rosters (team_ids); print (f'Got the team roster ids for {len(roster_ids)} players')
 
-player_stats =[]
+# player_stats =[]
 for i, r in enumerate(roster_ids):
     print (i)
-    player_stats.append(get_player_stats (r))
-    if i == 20:
-#         print (player_stats)
-        all_players = pd.DataFrame(player_stats)
-        make_web ('players', all_players)
+    player_stats = (get_player_info (r))
+#     print (get_player_info (r))
+#     if i == 20:
+# #         print (player_stats)
+#         all_players = pd.DataFrame(player_stats)
+#         make_web ('players', all_players)
