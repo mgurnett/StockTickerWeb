197548dedc55f7d8510ce9c83c849a6d7b0a8a6d
Michael Gurnett <mgurnett@gmail.com>
2021-03-06 09:46:23 -0700

    Changed the name of Django from weather and lookup to sports and NHL

--- sports/NHL/apps.py
+++ sports/NHL/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class LookupConfig(AppConfig):
-    name = 'lookup'
+    name = 'NHL'
--- sports/sports/settings.py
+++ sports/sports/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lookup',
+    'NHL',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'weather.urls'
+ROOT_URLCONF = 'sports.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'weather.wsgi.application'
+WSGI_APPLICATION = 'sports.wsgi.application'
 
 
 # Database
