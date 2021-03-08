969b093f0c733fac5f675cf7e7433c818dd9fb5c
Michael <mgurnett@gmail.com>
2021-03-08 12:37:40 -0700

    Removed a bunch of stuff

--- Django base setup/weather/lookup/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LookupConfig(AppConfig):
-    name = 'lookup'
--- sports/NHL/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LookupConfig(AppConfig):
-    name = 'NHL'
