3bccb512d55a822c0a7d0c8bcd5185bab6a671a4
Michael <mgurnett@gmail.com>
2021-03-17 14:49:58 -0600

    Got time fix working but not implemented

--- website/nhl/times.py
+++ website/nhl/times.py
@@ -1,35 +1,12 @@
-from datetime import datetime
 import pytz
+from datetime import datetime
 
-ED_tz = pytz.timezone("America/Edmonton")
-
-date_data = '2021-01-17T00:07:44Z'
-datetime_object = datetime.strptime(date_data, '%Y-%m-%dT%H:%M:%SZ')
-d = pytz.UTC.localize(datetime_object) #make it UTC
-print (f'd         {d}')
-d.astimezone(ED_tz)
-
-print (f'd         {d}')
+def fix_time (dt_object): 
+    print (f'dt_object {dt_object} of {type(dt_object)}')
+    UTC_time = pytz.timezone("UTC").localize(dt_object)
+    d = UTC_time.astimezone()
+    Edmonton_time = d.strftime("%d %b %Y (%I:%M:%S:%f %p) %Z") #Print it with a directive of choich
+    return (Edmonton_time)
 
 
-
-# print (f'datetime_object {datetime_object}')
-# # MT_time = datetime_object.astimezone(pytz.timezone("America/Edmonton"))
-# print (f'our_time         {our_time}')
-
-
-
-'''
-
-# print (datetime_object)
-
-date_time = timezone.localize(datetime_object)
-# date_time.tzinfo
-print (date_time.tzinfo)
-
-
-#2021-01-17 00:07:44
-
-
-
-'''
+            date_fixed = fix_time (datetime_object)
