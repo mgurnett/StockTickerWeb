13a08546a1989250a87e0df651bedcc39b58af1e
Michael Gurnett <mgurnett@gmail.com>
2021-04-06 22:00:58 -0600

    working on the new covid stuff

--- covid/data_clean.py
+++ covid/data_clean.py
@@ -1,54 +1,19 @@
 import pandas as pd
 
-def findStartZero (x):
-#     print ('X in findStartZero:', x)
-    max = len(cases.index)
-
-    while cases[x] > 0:
-
-#         print ('X in findStartZero - while loop:', x)
-
-        if x >= max-1:
-#             print ('Len of cases.index', max, 'AND X is', x)
-            return max
-        x += 1
-#     print ('X returned from findStartZero:', x)
-    return (x)
-
-def findEndZero (x):
-    # print ('X sent in:', x)
-    if x >= len(cases.index):
-#         print ('falling out')
-        return (x)
-    while cases[x] == 0:
-        x += 1
-#         print ('X in while loop:', x)
-    if x > len(cases.index): #this prevents running over the end.
-        x = len(cases.index)-1
-#     print ('X after if:', x)
-    return (x)
-
-def data_fill (first, last):
-#     print('=============', first)
-#     print ('first -1', first-1, cases[first-1])
-#     print ('first',  first, cases[first])
-#     print ('first+1',  first+1, cases[first+1])
-#     print ('first+2',  first+2, cases[first+2])
-#     print ('last',  last, cases[last])
-    if last == len(cases.index):
-        return
-    run = last-first + 1
-#     print ('run', run)
-
-    fill_amount = cases[last] / run
-    for fill in range ( first, last + 1 ):
-        cases[fill] = fill_amount
-    return
+def fill_zeros (data, first, last):
+    for x in range (len(first)):
+        num_of_days = ((last[x] - first[x]).days) + 1
+        total_date = last[x]
+        total_amount = data.at[total_date,'cases']
+        amount = int (total_amount / num_of_days)
+        print (f'first zero {first[x]} and ends at {total_date} or {num_of_days} days and the total is {total_amount} amount {amount}')
+        for y in range (number_of_days+1)
+    return (data)
 
 def remove_zeros (data):
     starts = []; ends =[]; prev_value = 0
     for index, row in data.iterrows():
-        print (row)
+        # print (row)
         if prev_value > 0 and row['cases'] == 0:
             prev_value = 0
             starts.append(index)
@@ -56,8 +21,7 @@ def remove_zeros (data):
             prev_value = row['cases']
             ends.append(index)
     ends.pop(0) #remove the first one as it is the end of the initial zeros.
-    print (f'starts {len(starts)} and ends {len(ends)}')
-
+    data = fill_zeros (data, starts, ends)
     return data
 
 if __name__ == '__main__':
